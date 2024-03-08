50a4800e14f9a7d56ecc03327b6b8c0c9026cc15
123rhkddms2 <123rhkddms2@gmail.com>
2024-03-08 17:51:33 +0900

    dd

--- ch05/src/main/java/kr/co/ch05/config/JdbcConfig.java
+++ ch05/src/main/java/kr/co/ch05/config/JdbcConfig.java
@@ -15,8 +15,13 @@ public class JdbcConfig {
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/studydb");
+        // 개발용
+        //dataSource.setUsername("123rhkddms2");
+        //dataSource.setPassword("1234");
+
+        // 배포용(AWS)
         dataSource.setUsername("123rhkddms2");
-        dataSource.setPassword("1234");
+        dataSource.setPassword("Rlarhkddms66@");
 
         // DBCP2 옵션 설정
         dataSource.setMaxTotal(13); // 최대 연결 풀 크기 설정
