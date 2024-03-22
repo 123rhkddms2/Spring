cfacd1a2737927a3bbf910460c4d869132c01ae7
123rhkddms2 <123rhkddms2@gmail.com>
2024-03-22 17:04:19 +0900

    sboard

--- sboard/src/main/java/kr/co/sboard/config/RootConfig.java
+++ sboard/src/main/java/kr/co/sboard/config/RootConfig.java
@@ -15,19 +15,28 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @Configuration
 @EnableAspectJAutoProxy
 public class RootConfig {
+
     // 빌드 정보 객체를 주입 받기 위해 build.gradle 파일 맨 밑에 buildInfo() 실행 해야됨
     @Autowired
     private BuildProperties buildProperties;
 
     @Bean
-    public ModelMapper modelMapper(){                   // 매핑 작업을 편리하게 수행하기 위해 modelMapper 인스턴스를 생성(DTO <> Entity 간 변환작업)
+    public AppInfo appInfo(){
+        String name = buildProperties.getName();
+        String version = buildProperties.getVersion();
+        return new AppInfo(name, version);
+    }
 
-        ModelMapper modelMapper = new ModelMapper();    // Entity의 @Setter 선언없이 바로 private 속성으로 초기화 설정
+    @Bean
+    public ModelMapper modelMapper(){
+
+        // Entity의 @Setter 선언없이 바로 private 속성으로 초기화 설정
+        ModelMapper modelMapper = new ModelMapper();
 
         modelMapper.getConfiguration()
-                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)      // private 필드에 접근할 수 있게함
-                .setMatchingStrategy(MatchingStrategies.STRICT)                                     // 매우 엄격한 매칭 전략을 사용
-                .setFieldMatchingEnabled(true);                                                     // 필드 매칭을 활성화
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldMatchingEnabled(true);
 
         return modelMapper;
     }
--- sboard/src/main/java/kr/co/sboard/service/CommentService.java
+++ sboard/src/main/java/kr/co/sboard/service/CommentService.java
@@ -21,68 +21,66 @@ public class CommentService {
     private final ArticleRepository articleRepository;
     private final ModelMapper modelMapper;
 
-    public ResponseEntity<Article> insertComment(ArticleDTO articleDTO) {
-        Article article = modelMapper.map(articleDTO, Article.class);           // articleDTO를 Article엔티티로 변환
-        Article savedArticle = articleRepository.save(article);                 // Article객체(savedArticle)을 ArticleRepository의 save메서드를 사용해 DB에 저장
+    public ResponseEntity<Article> insertComment(ArticleDTO articleDTO){
+        Article article = modelMapper.map(articleDTO, Article.class);
+        Article savedArticle = articleRepository.save(article);
 
-        return ResponseEntity.ok().body(savedArticle);                          // 저장된 Article객체(savedArticle)를 ResponseEntity에 담아서 반환
+        return ResponseEntity.ok().body(savedArticle);
     }
 
-    public ResponseEntity<List<ArticleDTO>> selectComments(int no) {            
+    public ResponseEntity<List<ArticleDTO>> selectComments(int no){
 
         // ArticleRepository > findByParent() 쿼리 메서드 정의
-        List<Article> articleList = articleRepository.findByParent(no);         // articleRepository.findByParent(no)를 호출해 `no`에 해당하는 글들을 DB에서 조회
+        List<Article> articleList = articleRepository.findByParent(no);
 
-        List<ArticleDTO> articleDTOS = articleList.stream()                     // 조회된 댓글(articleList)에 대해 스트림을 생성
-                .map(entity -> modelMapper.map(entity, ArticleDTO.class))       // Article 엔티티를 ArticleDTO로 매핑
-                .toList();                                                      // 변환된 ArticleDTO 객체들을 리스트로 수집
+        List<ArticleDTO> articleDTOS = articleList.stream()
+                .map(entity -> modelMapper.map(entity, ArticleDTO.class))
+                .toList();
 
-        return ResponseEntity.ok().body(articleDTOS);                           // 변환된 List<ArticleDTO>를 ResponseEntity에 담아서 반환
+        return ResponseEntity.ok().body(articleDTOS);
     }
 
-    public ResponseEntity<?> deleteComment(int no) {
+    public ResponseEntity<?> deleteComment(int no){
 
         log.info("no : " + no);
         // 삭제 전 조회
-        Optional<Article> optArticle = articleRepository.findById(no);          // articleRepository.findById(no)를 호출해 `no`에 해당하는 글들을 DB에서 조회
-                                                                                // Optional을 쓰는 이유는 댓글이 없을 수도 있기때문
+        Optional<Article> optArticle = articleRepository.findById(no);
+
         log.info("optArticle : " + optArticle);
 
-        if(optArticle.isPresent()){                                             // 조건문으로 Optional에 댓글이 있는지 확인
+        if(optArticle.isPresent()){
 
             log.info("here1");
 
-            articleRepository.deleteById(no);                                   // 있는경우 no에 해당하는 댓글 삭제
+            articleRepository.deleteById(no);
 
-            return ResponseEntity                                               // 삭제된 댓글에 대한 ResponEntity를 반환
+            return ResponseEntity
                     .ok()
                     .body(optArticle.get());
-        }else{                                                                  
-
+        }else{
             log.info("here2");
 
             return ResponseEntity
-                    .status(HttpStatus.NOT_FOUND)                               // 없는 경우 NOT_FOUND상태와 함께 메시지를 포함한 ResponseEntity를 반환
-                    .body("comment not found");
-
+                    .status(HttpStatus.NOT_FOUND)
+                    .body("not found");
         }
+
     }
 
     public ResponseEntity<?> updateComment(ArticleDTO articleDTO){
 
-        // 수정하기 전에 먼저 존재여부 확인(기존의 selectComment와 update기능을 다 담당하려는거같음)
+        // 수정하기 전에 먼저 존재여부 확인
         Optional<Article> optArticle = articleRepository.findById(articleDTO.getNo());
 
-        if(optArticle.isPresent()) {
+        if(optArticle.isPresent()){
 
-            Article article = optArticle.get();                                 // optArticle에서 실제 Article 객체를 가져옴
-            // 어쩔 수 없이 Article 엔티티에 @Setter 선언해서 수정하기
-            article.setContent(articleDTO.getContent());                        // 가져온 Article 객체의 내용을 업데이트
+            Article article = optArticle.get();
 
+            // 어쭬수 없이 Article 엔티티에 @Setter 선언해서 수정하기
+            article.setContent(articleDTO.getContent());
             log.info("article : " + article);
 
-
-            Article modifiedArticle = articleRepository.save(article);          // save로 해당 엔티티를 DB에 업데이트
+            Article modifiedArticle = articleRepository.save(article);
 
             // 수정 후 수정 데이터 반환
             return ResponseEntity
